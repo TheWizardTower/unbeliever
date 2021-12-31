9d8e34d508ef8d98ff74165738288ffadd6455e3
Andrew Cowie <istathar@gmail.com>
2021-12-31 16:02:02 +1100

    Begin wrapper library around Postgres access

---
+++ core-persistence-postgres/lib/Core/Persistence/Postgres.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+{- |
+Reading and writing data to and from a Postgres database.
+
+This module simply contains conveniences for using the most excellent
+__postgresql-simple__ library in concert with the rest of the packages in
+this collection.
+-}
+
+module Core.Persistence.Postgres (
+) where
