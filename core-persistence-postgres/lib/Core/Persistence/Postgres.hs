124157f867212f58e083291340f1e111d76a92be
Adam James McCullough <merlinfmct87@gmail.com>
2021-12-31 22:35:25 -0800

    Initial commit. This is the code that started it all.

--- core-persistence-postgres/lib/Core/Persistence/Postgres.hs
+++ core-persistence-postgres/lib/Core/Persistence/Postgres.hs
@@ -8,6 +8,15 @@ This module simply contains conveniences for using the most excellent
 __postgresql-simple__ library in concert with the rest of the packages in
 this collection.
 -}
-
 module Core.Persistence.Postgres (
-) where
+    ) where
+
+import Core.Text (Rope, fromRope)
+import Database.PostgreSQL.Simple.FromField (FromField, fromField)
+import Database.PostgreSQL.Simple.ToField (Action (..), ToField, toField)
+
+instance FromField Rope where
+    fromField f dat = fromRope <$> fromField f dat
+
+instance ToField Rope where
+    toField r = Escape $ fromRope r
