9c939ed2116c8e60d6c060a15907f18420b5b32e
Adam McCullough <merlinfmct87@gmail.com>
2022-01-14 23:55:42 -0800

    Add some query machinery!

--- core-persistence-postgres/lib/Core/Persistence/Postgres.hs
+++ core-persistence-postgres/lib/Core/Persistence/Postgres.hs
@@ -9,11 +9,21 @@ __postgresql-simple__ library in concert with the rest of the packages in
 this collection.
 -}
 module Core.Persistence.Postgres (
-    ) where
+    connectP,
+    getConnection,
+    getConnectionString,
+    newQuery,
+    newQuery_,
+    queryP,
+    queryP_,
+    refreshConnection,
+    setConnection,
+) where
 
+import Control.Monad.IO.Class (liftIO)
+import Core.Program (Program, getApplicationState, setApplicationState)
 import Core.Text (Rope, fromRope)
-import Core.Program (Program)
-import Database.PostgreSQL.Simple (Connection)
+import Database.PostgreSQL.Simple (Connection, ExecStatus (..), FromRow, Only (..), Query, SqlError (..), ToRow, connectPostgreSQL, query, query_)
 import Database.PostgreSQL.Simple.FromField (FromField, fromField)
 import Database.PostgreSQL.Simple.ToField (Action (..), ToField, toField)
 
@@ -27,3 +37,44 @@ class Database d where
     getConnection :: d -> Program d Connection
     setConnection :: Connection -> d -> Program d d
     getConnectionString :: d -> Program d Rope
+
+queryP :: (ToRow q, FromRow r) => Connection -> Query -> q -> Program e [r]
+queryP conn q args = liftIO $ query conn q args
+
+queryP_ :: (FromRow r) => Connection -> Query -> Program e [r]
+queryP_ conn q = liftIO $ query_ conn q
+
+newQuery :: (Database d, ToRow q, FromRow r) => Query -> q -> Program d [r]
+newQuery q args = do
+    env <- getApplicationState
+    conn <- getConnection env
+    queryP conn q args
+
+newQuery_ :: (Database d, FromRow r) => Query -> Program d [r]
+newQuery_ q = do
+    env <- getApplicationState
+    conn <- getConnection env
+    queryP_ conn q
+
+connectP :: Rope -> Program d Connection
+connectP connStr = liftIO $ connectPostgreSQL $ fromRope connStr
+
+refreshConnection :: (Database d) => Program d Connection
+refreshConnection = do
+    env <- getApplicationState
+    connStr <- getConnectionString env
+    conn <- connectP connStr
+    env' <- setConnection conn env
+    setApplicationState env'
+    return conn
+
+connectionDisconnected :: SqlError
+connectionDisconnected =
+    SqlError
+        { sqlState = ""
+        , sqlExecStatus = FatalError
+        , sqlErrorMsg = "connection disconnected"
+        , sqlErrorDetail = ""
+        , sqlErrorHint = ""
+        }
+
